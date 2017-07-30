57801809d1f1ce190140b6b750e0545a3618ae6f
Amio <amio.cn@gmail.com>
2017-07-30 21:28:31 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const marked = require('marked')
+const { json, send } = require('micro')
+
+const README = marked(fs.readFileSync('README.md', 'utf-8'))
+const IndexHTML = helmet(README, {
+  title: 'Marked: markdown render service.',
+  linkCSS: 'https://markdowncss.github.io/splendor/css/splendor.css'
+})
+
+const endpoint = async (req, res) => {
+  if (req.method === 'POST' && req.url === '/') {
+    const { text, title, linkCSS, inlineCSS } = await json(req)
+
+    if (text) {
+      res.setHeader('Content-Type', 'text/html; charset=utf-8')
+      return helmet(marked(text), { title, linkCSS, inlineCSS })
+    } else {
+      return send(res, 400, 'Bad Request')
+    }
+  }
+
+  if (req.method === 'GET') {
+    switch (req.url) {
+      case '/favicon.ico':
+        return ''
+      default:
+        return IndexHTML
+    }
+  }
+}
+
+function helmet (bodyHTML, { title = '', linkCSS, inlineCSS }) {
+  const linkStyle = linkCSS ? `<link rel="stylesheet" href="${linkCSS}" />` : ''
+  const inlineStyles = inlineCSS ? `<style>${inlineCSS}</style>` : ''
+
+  return `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <title>${title}</title>
+        ${linkStyle}
+        ${inlineStyles}
+      </head>
+      <body>${bodyHTML}</body>
+    </html>
+  `
+}
+
+module.exports = endpoint
+require('micro')(module.exports).listen(3000)
